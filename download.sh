c3e1f97648e9088fa883594278745945a1db4307
Remimstr <remievan@hotmail.com>
2016-07-12 14:31:21 -0400

    Make download_Salmonella.sh universal and rename to download.sh

--- download.sh
+++ download.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-currentSeconds=$(date -j -f "%Y-%m-%d" $1 "+%s")
-endSeconds=$(date -j -f "%Y-%m-%d" $2 "+%s")
+currentSeconds=$(date -j -f "%Y-%m-%d" $2 "+%s")
+endSeconds=$(date -j -f "%Y-%m-%d" $3 "+%s")
 offset=518400
 increment=86400
 
-# Downloads Salmonella metadata using metadata.py in 1-week increments,
-# starting at $1 and going to $2
+# Downloads metadata using metadata.py in 1-week increments,
+# starting at $2 and going to $3. Specify the organism name with $1
 while [ "$currentSeconds" -le "$endSeconds" ]
 do
   currentDateTs=$(date -j -f "%s" $currentSeconds "+%Y-%m-%d")
   nextSeconds=$(($currentSeconds+$offset))
   nextDateTs=$(date -j -f "%s" $nextSeconds "+%Y-%m-%d")
-  metadata.py --name Salmonella --start $currentDateTs --end $nextDateTs --time 3.0
+  metadata.py --name $1 --start $currentDateTs --end $nextDateTs --time 3.0
   currentSeconds=$(($nextSeconds+$increment))
   sleep 3.0
 done
